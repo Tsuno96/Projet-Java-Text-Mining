95a259dc7f88328db1ec645f5a7f41e88f7c5308
Mathieu <tsunoll96@gmail.com>
2019-11-30 18:36:31 +0100

    WOW On a une list de lignes

--- src/TextMining.java
+++ src/TextMining.java
@@ -1,34 +1,44 @@
 // Java Program to illustrate reading from
 // FileReader using FileReader
+
 import java.io.*;
-    public class TextMining
-    {
-        public static void main(String[] args) throws Exception
-        {
-            // Importation de text
-            FileReader foot =
-                    new FileReader(".\\data\\Foot.txt");
-            BufferedReader tampon = new BufferedReader(foot);
+import java.util.*;
 
-            LineNumberReader count = new LineNumberReader(foot);
+public class TextMining {
+    public static void main(String[] args) throws Exception {
+        // Importation de text
+        FileReader foot =
+                new FileReader(".\\data\\Foot.txt");
+        BufferedReader tampon = new BufferedReader(foot);
 
-            // Afficher le 1er ligne
-            String ligne = tampon.readLine();
-            // Afficher le n-eme ligne
+        LineNumberReader count = new LineNumberReader(foot);
 
 
-            // Afficher le nombre de ligneù
-            int result = 0;
-            while(count.skip(Long.MAX_VALUE)>0) {
-                result = count.getLineNumber() +1; //Comme le 1er ligne = 0
-            }
+        //Liste des lignes
+        List<String> lstStrLignes = stock(tampon);
 
-            System.out.println(result);
-            System.out.println(ligne);
+        System.out.println(lstStrLignes.get(10));
 
+        // Afficher le nombre de ligneù
+        int result = 0;
+        while (count.skip(Long.MAX_VALUE) > 0) {
+            result = count.getLineNumber() + 1; //Comme le 1er ligne = 0
         }
+
+        System.out.println(result);
 
 
 
     }
+
+    public static List<String> stock (BufferedReader br) throws IOException {
+        List<String> lstStr = new ArrayList<String>();
+        String str;
+        while (( str = br.readLine()) != null) {
+            lstStr.add(str);
+        }
+        return lstStr;
+    }
+
+}
 
